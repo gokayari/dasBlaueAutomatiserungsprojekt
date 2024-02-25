c384f4b0df335a2669359feeda05454aac135621
gokayari <gokayari@gmail.com>
2024-02-25 12:39:27 +0100

    first commit

---
+++ src/main/java/org/Rechner.java
@@ -0,0 +1,20 @@
+package org;
+
+public class Rechner {
+
+    public double addition(double a, double b){
+        return a+b;
+    }
+
+    public double subtraktion(double a, double b){
+        return a-b;
+    }
+
+    public double multiplikation(double a, double b){
+        return a*b;
+    }
+
+    public double division(double a, double b){
+        return a/b;
+    }
+}
